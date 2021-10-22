0acdf27ef7310ce2a249bc279bcac922b753854f
wangyu19810820 <gadfly0036_cn@sina.com>
2021-10-22 13:54:52 +0800

    aa

---
+++ iOS开发零基础教程/URLSession/URLSession/News.swift
@@ -0,0 +1,37 @@
+//
+//  News.swift
+//  JSON
+//
+//  Created by wang yu on 2021/10/15.
+//
+
+import UIKit
+
+class News: Codable {
+    var title: String
+    var content: String
+    var readCount: Int {
+        get { read_count }
+        set { read_count = newValue }
+    }
+    private var read_count: Int
+    
+    var isBig: Bool {
+        get { is_big ?? false }
+        set { is_big = newValue }
+    }
+    private var is_big: Bool?
+}
+
+class NewsList: Codable {
+    var page: Int
+    var next: Bool
+    var list: [News]
+}
+
+extension NewsList {
+    var displayText: String {
+        let strings = list.map{ $0.title }
+        return strings.joined(separator: "\n")
+    }
+}
---
+++ iOS开发零基础教程/URLSession/URLSession/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  URLSession
+//
+//  Created by wang yu on 2021/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private var label: UILabel!
+    private var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let vStack = UIStackView()
+        vStack.translatesAutoresizingMaskIntoConstraints = false
+        vStack.backgroundColor = .systemGray5
+        vStack.axis = .vertical
+        view.addSubview(vStack)
+        
+        NSLayoutConstraint.activate([
+            vStack.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor),
+            vStack.rightAnchor.constraint(lessThanOrEqualTo: view.rightAnchor),
+            vStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            vStack.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+        
+        label = UILabel()
+        label.font = .systemFont(ofSize: 30)
+        label.numberOfLines = 0
+        vStack.addArrangedSubview(label)
+        
+        button = UIButton()
+        button.titleLabel?.font = .boldSystemFont(ofSize: 30)
+        button.setTitleColor(button.tintColor, for: .normal)
+        button.setTitle("Send", for: .normal)
+        button.addTarget(self, action: #selector(clickButton), for: .touchUpInside)
+        vStack.addArrangedSubview(button)
+        
+    }
+
+    @objc private func clickButton() {
+        let url = URL(string: "https://raw.githubusercontent.com/xiaoyouxinqing/Learn-iOS-from-Zero/main/API/NewsExample/NewsList.json")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+//        request.addValue("asdfsdaf", forHTTPHeaderField: "Token")
+//        request.httpMethod = "POST"
+//        let dic = ["page": 1]
+//        let data = try! JSONSerialization.data(withJSONObject: dic, options: [])
+//        request.httpBody = data
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                self.updateText(error.localizedDescription)
+                return
+            }
+            guard let response = response as? HTTPURLResponse,
+                response.statusCode >= 200,
+                response.statusCode < 300 else {
+                    self.updateText("Invalid response")
+                return
+            }
+            guard let data = data else {
+                self.updateText("No data")
+                return
+            }
+            guard let list = try? JSONDecoder().decode(NewsList.self, from: data) else {
+                self.updateText("No list")
+                return
+            }
+            self.updateText(list.displayText)
+        }
+        task.resume()
+    }
+    
+    private func updateText(_ text: String) {
+        DispatchQueue.main.async {
+            self.label.text = text
+        }
+    }
+}
+
